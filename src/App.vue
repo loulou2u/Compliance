674a3190fffc8ecdf1c33cc7a9770659418ce093
Laura McCord <lauramccord705@gmail.com>
2019-01-31 14:36:25 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <i class="material-icons icon-image-list">view_list</i> <Compliance />
+  </div>
+</template>
+
+<script>
+import Compliance from "./components/Compliance";
+
+export default {
+  name: "App",
+  components: {
+    Compliance
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.icon-image-list {
+  font-size: 64px;
+}
+</style>
+
---
+++ src/components/Compliance.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <h2>Compliance Requirements</h2>
+    <ul v-if="reqs && reqs.length">
+      <li v-for="(req, index) in reqs" :key="index">
+        <div>
+          <span v-if="req.status === 'true'"
+            ><i class="material-icons">check_box</i></span
+          >
+          <span v-else
+            ><i class="material-icons"> check_box_outline_blank</i></span
+          >
+          {{ req.title }}
+        </div>
+      </li>
+    </ul>
+
+    <ul v-if="errors && errors.length">
+      <li v-for="(error, index) in errors" :key="index">{{ error.message }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Compliance",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      reqs: [
+        { id: 1, title: "Animal User Training", status: "false" },
+        { id: 2, title: "Biosafety Training", status: "true" },
+        { id: 3, title: "Research Ethics", status: "true" },
+        { id: 4, title: "Research Security", status: "false" }
+      ],
+      errors: []
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  max-width: 300px;
+  margin: auto;
+}
+li {
+  border: none;
+  width: 300px;
+  text-align: left;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
